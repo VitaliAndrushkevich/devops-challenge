ef5a3904ce0de6dd5aa68b0a6c36f37faab19cf9
Vitali Andrushkevich <46499807+Vitalyazavr@users.noreply.github.com>
2021-08-20 19:27:02 +0300

    Terraform format fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
 
-    backend "s3" {
-      bucket = "devops-challenge-tfstate"
-      key    = "lambda-challenge/terraform.tfstate"
-      region = "us-east-2"
-    }
+  backend "s3" {
+    bucket = "devops-challenge-tfstate"
+    key    = "lambda-challenge/terraform.tfstate"
+    region = "us-east-2"
+  }
 
 }
